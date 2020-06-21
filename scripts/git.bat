d0dc2e87af69440820e1e4aa26c90a62bdedf596
Dennis Pham <dennis@dennispham.me>
2020-06-21 17:29:32 -0400

    use origin instead of project name

--- scripts/git.bat
+++ scripts/git.bat
@@ -3,5 +3,5 @@ set arg2=%2
 set arg3=%3
 
 cd %1
-git remote add %2 https://github.com/%3/%2.git
-git push --set-upstream %2 master
+git remote add origin https://github.com/%3/%2.git
+git push --set-upstream origin master
